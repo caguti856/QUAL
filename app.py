29e547bf039ffe9930baca95077e791a3646fcec
Aguti Catherine <114284982+caguti856@users.noreply.github.com>
2025-11-06 15:46:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ st.set_page_config(page_title="Thematic Analytics",  layout="wide")
 import advisory  # must define advisory.main()
 import thoughtleadership
 import growthmindset
+import networking
+import influencingrelationship
 # ----- optional cover -----
 COVER_HTML = """
 <div style="font-family: Inter, sans-serif; background:#f6f7f8; height: 100vh; display:flex; flex-direction:column; justify-content:space-between; text-align:center; color:#101922; padding:2rem 3rem;">
   <div>
-    <h1 style="font-size:2.8rem; font-weight:900; margin-bottom:.75rem;">Streamline Your Data Workflow</h1>
+    <h1 style="font-size:2.8rem; font-weight:900; margin-bottom:.75rem;">SCORING DASHBOARD</h1>
     <p style="font-size:1.1rem; color:#4b5563; max-width:600px; margin:0 auto 1.8rem; line-height:1.5;">
-      Powerful tools for data cleaning, database management, and insightful analytics.
+      SCORING QUALITATIVE DATA
     </p>
   </div>
   <footer style="font-size:.85rem; color:#777; margin-top:1rem;">© Thematic Analytics CARE UGANDA. All rights reserved.</footer>
@@ -38,7 +40,7 @@ def main() -> None:
     with st.sidebar:
         selected = st.selectbox(
             "Navigation",
-            options=["Advisory", "Thought Leadership","Growth Mindset"],  # <-- SINGLE list
+            options=["Advisory", "Thought Leadership","Growth Mindset","Networking","Influencing Relationship"],  # <-- SINGLE list
             index=0,
         )
 
@@ -48,6 +50,10 @@ def main() -> None:
     elif selected == "Thought Leadership":
         thoughtleadership.main()
     elif selected == "Growth Mindset":
-        growthmindset.main()  
+        growthmindset.main() 
+    elif selected == "Networking":
+        networking.main() 
+    elif selected == "Influencing Relationship":
+        influencingrelationship.main()  
 if __name__ == "__main__":
     main()
